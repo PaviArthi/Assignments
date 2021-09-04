d87e5ce5313615a525359f49fe82b4833febf7fd
PaviArthi <90091549+PaviArthi@users.noreply.github.com>
2021-09-04 17:55:01 +0530

    Checking Prime Number or Not

---
+++ PrimeNumber.java
@@ -0,0 +1,27 @@
+package assignments;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=13;
+		boolean flag=false;
+		for(int i=2;i<=num/2;i++)
+		{
+			if(num%i==0)
+			{
+				flag=true;
+				break;
+			}
+		}
+		if(flag==false)
+		{
+			System.out.println("Prime");
+		}
+			else
+			{
+				System.out.println("Not a Prime");
+	}
+	}
+
+}
