83ca6c0339e8e3f88526075efc7edaf36bd96b8e
PaviArthi <90091549+PaviArthi@users.noreply.github.com>
2021-09-04 17:50:38 +0530

    Add files via upload
    Checking Positive or Negative Number

---
+++ CheckPositiveNegative.java
@@ -0,0 +1,22 @@
+package assignments;
+
+public class CheckPositiveNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=35;
+		if(number>0)
+		{
+			System.out.println("The Number is Positive");
+		}
+		else if(number<0)
+		{
+			System.out.println("The Number is Negative");
+		}
+		else
+		{
+		System.out.println("The Number is neither positive or negative");
+		}
+	}
+
+}
