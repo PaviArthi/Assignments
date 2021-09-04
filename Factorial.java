a14761dbb38332eccbc039bb0b20d622c56fa671
PaviArthi <90091549+PaviArthi@users.noreply.github.com>
2021-09-04 19:14:58 +0530

    Find Factorial Number

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+package assignments;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 5,fact = 1;; 
+		 
+		for (int i = 1; i <= number; i++) 
+		{ 
+			fact = fact * i;
+		}
+		System.out.println(fact);
+	}
+
+}
