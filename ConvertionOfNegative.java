d926fd86f9360b3bd357ecb9a6f0dd2cf83660f1
PaviArthi <90091549+PaviArthi@users.noreply.github.com>
2021-09-04 19:07:11 +0530

    Convertion of Negative to Positive Number

---
+++ ConvertionOfNegative.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class ConvertionOfNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=-40;
+		if(number<0)
+		{
+		
+			System.out.println("The Number "+number+" is converted to "+(-(number)));
+		}
+	}
+
+}
